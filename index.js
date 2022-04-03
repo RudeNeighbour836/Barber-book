677ea1d371ca3a266b11bb2349a1bb9a0bd8745a
William <57761979+mungutiwilliam@users.noreply.github.com>
2022-04-03 18:18:30 +0300

    Update index.js
    added post method

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
+const fs = require('fs');
+const filename = 'details.json';
 const urlEncodedParser = bodyParser.urlencoded({extended: false});
+
+
+let rawData = fs.readFileSync(filename);
+let data = JSON.parse(rawData);
 
 app.set('views','pages');
 app.set('view engine', 'hbs');
@@ -11,6 +17,28 @@ app.use(express.static('public'));
 
 app.get('/', function(request, response){
     response.render('Barber_Black_Sheep');
+});
+
+
+app.get('/book', function(request, response){
+    data.sort((a, b) =>(a.name > b.name)? 1 : -1);
+    response.send(data);
+});
+
+app.post('/book', urlEncodedParser,function(request, response){
+    
+    const fs = require('fs');
+    const path = require('path');
+
+    let newbooking =
+        {
+            service: request.body.service,
+            date: request.body.date,
+        };
+
+    data.push(newbooking);
+    fs.writeFileSync(filename,JSON.stringify(data, null, 2));
+    response.render('booking_confirmation', newbooking);
 });
 
 app.listen(port);
