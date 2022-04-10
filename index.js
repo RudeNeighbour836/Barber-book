5db5d6d41d57e9237307aa999dbf9d4c97a0a465
Caesar Mwangi <czarmwas@gmail.com>
2022-04-10 22:30:35 +0300

    Fixed more errors js

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+http = require('http')
+const bcrypt = require('bcrypt')
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+const methodOverride = require('method-override')
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
@@ -5,6 +14,12 @@ const bodyParser = require('body-parser');
 const fs = require('fs');
 const filename = 'details.json';
 const urlEncodedParser = bodyParser.urlencoded({extended: false});
+const initializePassport = require('./passport-config').initialize
+initializePassport(
+  passport,
+  email => users.find(user => user.email === email),
+  id => users.find(user => user.id === id)
+)
 
 let rawData = fs.readFileSync(filename);
 let data = JSON.parse(rawData);
@@ -14,13 +29,38 @@ app.set('views','pages');
 app.set('view engine', 'hbs');
 app.use(express.static('public'));
 
-
 app.get('/', function(request, response){
     response.render('Barber_Black_Sheep');
 });
 app.get('/view_booking', function(request, response){
     response.render('view_reservation');
 });
+
+const users = []
+
+app.set('view-engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(flash())
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride('_method'))
+
+app.get('/login', checkNotAuthenticated, (req, res) => {
+  res.render('login.ejs')
+})
+
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+  successRedirect: '/index',
+  failureRedirect: '/login',
+  failureFlash: true
+}))
+
 
 app.get('/book', function(request, response){
     data.sort((a, b, c, d) =>(a.name > b.name > c.name > d.name)? 1 : -1);
@@ -69,91 +109,55 @@ console.log(`Server is listening on port ${port}`);
 
 
 
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
-  }
-  
-  
-  http = require('http')
-  const bcrypt = require('bcrypt')
-  const passport = require('passport')
-  const flash = require('express-flash')
-  const session = require('express-session')
-  const methodOverride = require('method-override')
-  
-  const initializePassport = require('./passport-config').initialize
-  initializePassport(
-    passport,
-    email => users.find(user => user.email === email),
-    id => users.find(user => user.id === id)
-  )
-  
-  const users = []
-  
-  app.set('view-engine', 'ejs')
-  app.use(express.urlencoded({ extended: false }))
-  app.use(flash())
-  app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false
-  }))
-  
-  app.use(passport.initialize())
-  app.use(passport.session())
-  app.use(methodOverride('_method'))
-  
-  app.get('/index', checkAuthenticated, (req, res) => {
-    res.render('index.ejs', { name: req.user.name })
-  })
-  app.get('/login', checkNotAuthenticated, (req, res) => {
-    res.render('login.ejs')
-  })
-  
-  app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
-    successRedirect: '/index',
-    failureRedirect: '/login',
-    failureFlash: true
-  }))
-  
-  app.get('/register', checkNotAuthenticated, (req, res) => {
-    res.render('register.ejs')
-  })
-  
-  app.post('/register', checkNotAuthenticated, async (req, res) => {
-    try {
-      const hashedPassword = await bcrypt.hash(req.body.password, 10)
-      users.push({
-        id: Date.now().toString(),
-        name: req.body.name,
-        email: req.body.email,
-        password: hashedPassword
-      })
-      res.redirect('/login')
-    } catch {
-      res.redirect('/register')
-    }
-  })
-  
-  app.delete('/logout', (req, res) => {
-    req.logOut()
+
+
+
+
+
+
+
+app.get('/index', checkAuthenticated, (req, res) => {
+  res.render('index.ejs', { name: req.user.name })
+})
+
+app.get('/register', checkNotAuthenticated, (req, res) => {
+  res.render('register.ejs')
+})
+
+app.post('/register', checkNotAuthenticated, async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword
+    })
     res.redirect('/login')
-  })
-  
-  function checkAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-      return next()
-    }
-  
-    res.redirect('/login')
+  } catch {
+    res.redirect('/register')
   }
-  
-  function checkNotAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-      return res.redirect('/login')
-    }
-    next()
+})
+
+app.delete('/logout', (req, res) => {
+  req.logOut()
+  res.redirect('/login')
+})
+
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next()
   }
-  
-  app.listen(3001)
-  console.log('listening to port 3001')
+
+  res.redirect('/login')
+}
+
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect('/login')
+  }
+  next()
+}
+
+app.listen(3001)
+console.log('listening to port 3001')
